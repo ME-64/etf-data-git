115f3ae485bdd3231a4a696a8d774f3a4ba6c2c6
ME-64 <ME-64>
2020-01-04 10:09:17 +0000

    fixed path reference for fx_api access_key using pathlib

--- etf-data/etf_data/fx_api.py
+++ etf-data/etf_data/fx_api.py
@@ -1,38 +1,41 @@
 import requests
 import urllib
 import pandas as pd
-
+import os
+from pathlib import Path
 
 def get_fx(quoted, dates, base='USD', main_api ='http://data.fixer.io/api/', key=None):
 
-"""Returns historical FX rates from the fixer.io api for given currencies on a specified date
+    '''
+    Returns historical FX rates from the fixer.io api for given currencies on a specified date
 
-Parameters
-----------
-quoted : string or list
-    the currency or currencies to return a rate for.
-    Specified with 3 letter currency codes (i.e. "GBP")
+    Parameters
+    ----------
+    quoted : string or list
+        the currency or currencies to return a rate for.
+        Specified with 3 letter currency codes (i.e. "GBP")
 
-dates: string or list
-    the date or dates to return the rate for.
-    Specified in "YYY-MM-DD" format
+    dates: string or list
+        the date or dates to return the rate for.
+        Specified in "YYY-MM-DD" format
 
-base: string
-    the currency used to compare the quoted currencies too
-    Specified with 3 letter currency codes
+    base: string
+        the currency used to compare the quoted currencies too
+        Specified with 3 letter currency codes
 
-Returns
--------
-pandas.DataFrame containing exchange rates for each specified date & currency
-"""
-
-    # Read in API access key
+    Returns
+    -------
+    pandas.DataFrame containing exchange rates for each specified date & currency
+    '''
     try:
-        f = open('../data/access_key.txt')
+        # Read in API access key when script or import
+        path = Path(__file__).absolute().parent.parent.joinpath('data', 'access_key.txt')
+        f = open(path)
         key = f.read()
-    except FileNotFoundError as e:
-        raise FileNotFoundError(
-        'Please sign-up for fixer.io and create file w/APIkey in "data/access_key.txt"') from e
+        
+    except FileNotFoundError as err:
+        print(Path(__file__).absolute())
+        raise FileNotFoundError('Please sign-up for fixer.io and create file w/APIkey in "data/access_key.txt"') from err
     except Exception:
         raise
     else:
@@ -79,3 +82,7 @@ pandas.DataFrame containing exchange rates for each specified date & currency
         new_rates_df['date'] = new_rates_df['date'].astype(pd.np.datetime64)
 
     return new_rates_df
+
+
+if __name__ == '__main__':
+    print(get_fx('EUR', '2019-12-30'))
